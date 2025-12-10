aecd6601aba61fa427be8c71293bb57d9ace5c5f
Vincent Lau <lauvincent1031@gmail.com>
2025-12-09 18:53:37 -0800

    feat: integrate Vue.js and configure Axios with Laravel Echo for real-time functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+const mountElement = document.getElementById('app');
+if (mountElement) {
+    app.mount('#app');
+}
